db2fe39d51583bee83771c794ded368023b53daa
Craig Swank <craigswank@gmail.com>
2024-02-21 06:00:00 -0700

    unset interrupt while processing ir data

--- main.go
+++ main.go
@@ -4,7 +4,7 @@ import (
 	"machine"
 	"time"
 
-	necir "github.com/cswank/amplifier/internal/ir"
+	"github.com/cswank/amplifier/internal/ir"
 )
 
 type (
@@ -12,50 +12,42 @@ type (
 )
 
 func main() {
-
 	led := machine.LED
 	led.Configure(machine.PinConfig{Mode: machine.PinOutput})
-
-	irCh := make(chan empty)
-	ir := machine.GPIO15
-	ir.Configure(machine.PinConfig{Mode: machine.PinInput})
-	go wait(ir, irCh)
 
 	led.High()
 	time.Sleep(250 * time.Millisecond)
 	led.Low()
 
-	tk := time.NewTicker(250 * time.Millisecond)
+	irPin := machine.GPIO15
+	irPin.Configure(machine.PinConfig{Mode: machine.PinInput})
+
 	events := make([]time.Time, 200)
-	var addr, cmd uint8
-	var err error
 	var i int
-	for {
-		select {
-		case <-tk.C:
-			if i == 0 || time.Now().Sub(events[i-1]) < 100*time.Millisecond {
-				continue
-			}
 
-			addr, cmd, err = necir.Command(events[:i])
-			if err != nil {
-				blink(led, 5)
-			} else {
-				if addr == 0x35 && cmd == 0x40 {
-					blink(led, 2)
-				} else {
-					blink(led, 1)
-				}
-			}
-			i = 0
-		case <-irCh:
-			if i == 199 {
-				continue
-			}
-
-			events[i] = time.Now()
-			i++
+	irEvents := func(p machine.Pin) {
+		if i == 199 {
+			return
 		}
+
+		events[i] = time.Now()
+		i++
+	}
+
+	irPin.SetInterrupt(machine.PinToggle, irEvents)
+
+	tk := time.NewTicker(250 * time.Millisecond)
+
+	for {
+		<-tk.C
+		if i < 66 || time.Now().Sub(events[i-1]) < 100*time.Millisecond {
+			continue
+		}
+
+		irPin.SetInterrupt(0, nil)
+		parseIR(i, events, led)
+		i = 0
+		irPin.SetInterrupt(machine.PinToggle, irEvents)
 	}
 }
 
@@ -68,26 +60,15 @@ func blink(led machine.Pin, n int) {
 	}
 }
 
-func wait(gpio machine.Pin, ch chan empty) {
-	gpio.SetInterrupt(machine.PinToggle, func(p machine.Pin) {
-		ch <- empty{}
-	})
-}
-
-type timer struct {
-	t    *time.Timer
-	recv bool
-}
-
-func newTimer(d time.Duration) *timer {
-	return &timer{t: time.NewTimer(d)}
-}
-
-func (t *timer) reset(d time.Duration) {
-	if !t.t.Stop() {
-		if !t.recv {
-			<-t.t.C
+func parseIR(i int, events []time.Time, led machine.Pin) {
+	addr, cmd, err := ir.Command(events[:i])
+	if err != nil {
+		blink(led, 5)
+	} else {
+		if addr == 0x35 && cmd == 0x40 {
+			blink(led, 2)
+		} else {
+			blink(led, 1)
 		}
 	}
-	t.t.Reset(d)
 }
