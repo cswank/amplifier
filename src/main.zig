0e9b62938d975ddd8d6e53533b429de552c105ee
Craig Swank <craigswank@gmail.com>
2025-12-14 09:57:47 -0700

    add the rest of the gpio pins

--- src/main.zig
+++ src/main.zig
@@ -7,9 +7,12 @@ const rptime = rp2xxx.time;
 const time = microzig.drivers.time;
 const gpio = rp2xxx.gpio;
 
-const button = gpio.num(9);
-const irPin = gpio.num(10);
 const led = gpio.num(25);
+const button = gpio.num(12);
+const ir_input = gpio.num(15);
+const pwr = gpio.num(20);
+const on_led = gpio.num(17);
+const off_led = gpio.num(16);
 
 const uart = rp2xxx.uart.instance.num(0);
 const tx_pin = gpio.num(0);
@@ -33,16 +36,10 @@ fn callback() linksection(".ram_text") callconv(.c) void {
                 led.toggle();
                 std.log.debug("button fall: {}, rise: {}", .{ e.events.fall, e.events.rise });
             },
-            irPin => {
+            ir_input => {
                 t2 = rptime.get_time_since_boot();
                 if (parser.put(t2.diff(t1).to_us())) {
-                    if (parser.value()) |msg| {
-                        if (msg.address == 0x35 and msg.command == 0x40) {
-                            led.toggle();
-                        }
-                    } else |_| {
-                        blink();
-                    }
+                    checkIR();
                 }
                 t1 = t2;
             },
@@ -51,8 +48,26 @@ fn callback() linksection(".ram_text") callconv(.c) void {
     }
 }
 
-fn blink() void {
-    for (0..5) |_| {
+fn togglePower() void {
+    const st = pwr.read();
+    pwr.put(1 - st);
+    on_led.put(1 - st);
+    off_led.put(st);
+    blink(2);
+}
+
+fn checkIR() void {
+    if (parser.value()) |msg| {
+        if (msg.address == 0x35 and msg.command == 0x40) {
+            togglePower();
+        }
+    } else |_| {
+        blink(5);
+    }
+}
+
+fn blink(n: u4) void {
+    for (0..n * 2) |_| {
         led.toggle();
         rptime.sleep_ms(250);
     }
@@ -72,15 +87,21 @@ fn init() void {
     button.set_pull(.down);
     button.set_irq_enabled(gpio.IrqEvents{ .fall = 0, .rise = 1 }, true);
 
-    irPin.set_function(.sio);
-    irPin.set_direction(.in);
-    irPin.set_pull(.up);
-    irPin.set_irq_enabled(gpio.IrqEvents{ .fall = 1, .rise = 1 }, true);
+    ir_input.set_function(.sio);
+    ir_input.set_direction(.in);
+    ir_input.set_pull(.up);
+    ir_input.set_irq_enabled(gpio.IrqEvents{ .fall = 1, .rise = 1 }, true);
 
     microzig.interrupt.enable(.IO_IRQ_BANK0);
 
     led.set_function(.sio);
     led.set_direction(.out);
+    on_led.set_function(.sio);
+    on_led.set_direction(.out);
+    off_led.set_function(.sio);
+    off_led.set_direction(.out);
+    pwr.set_function(.sio);
+    pwr.set_direction(.out);
 
     tx_pin.set_function(.uart);
 
